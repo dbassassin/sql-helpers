5727f90061894f17c638af6ad9d649568061ef16
Tim M. Hidalgo <tim@crmls.org>
2015-04-09 13:02:48 -0700

    Added Example Tail Backup
    This script is simply an example for using Ola Hallengren's SQL Server
    Backup to perform a Tail Backup.

---
+++ backup/Example Tail Backup.sql
@@ -0,0 +1,56 @@
+USE [msdb]
+GO
+
+/****** Object:  Job [DatabaseBackup - SINGLE DB TAIL LOG]    Script Date: 4/9/2015 12:59:23 PM ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [Database Maintenance]    Script Date: 4/9/2015 12:59:23 PM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'Database Maintenance' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'Database Maintenance'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'DatabaseBackup - SINGLE DB TAIL LOG', 
+		@enabled=1, 
+		@notify_level_eventlog=2, 
+		@notify_level_email=2, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'Source: http://ola.hallengren.com', 
+		@category_name=N'Database Maintenance', 
+		@owner_login_name=N'tanderson', 
+		@notify_email_operator_name=N'Database Admin', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [DatabaseBackup - SINGLE DB TAIL LOG]    Script Date: 4/9/2015 12:59:24 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'DatabaseBackup - SINGLE DB TAIL LOG', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'CmdExec', 
+		@command=N'sqlcmd -E -S $(ESCAPE_SQUOTE(SRVR)) -d master -Q "EXECUTE [dbo].[DatabaseBackup] @Databases = ''AdventureWorks2014'', @Directory = N''\\SANDBOX-PUB2\SQLBACKUP'', @BackupType = ''LOG'', @Verify = ''Y'', @NoRecovery = ''Y'', @CheckSum = ''Y'', @LogToTable = ''Y''" -b', 
+		@output_file_name=N'D:\SQLDATA\MSSQL11.MSSQLSERVER\MSSQL\LOG\DatabaseBackup_$(ESCAPE_SQUOTE(JOBID))_$(ESCAPE_SQUOTE(STEPID))_$(ESCAPE_SQUOTE(STRTDT))_$(ESCAPE_SQUOTE(STRTTM)).txt', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+
+GO
+
+
