e39737997b6eef8cd4d3b829f8e099ac4f868e34
Tim M. Hidalgo <tim.hidalgo@gmail.com>
2017-03-22 13:50:49 -0700

    Cleaning up directories

--- indexing/Find Hypothetical Indexes.sql
+++
@@ -1,5 +0,0 @@
-SELECT 'DROP INDEX [' + i.NAME + '] ON [' + s.NAME + '].[' + t.NAME + ']' AS [Drop TSQL]
-FROM sys.indexes i
-INNER JOIN sys.tables t ON i.object_id = t.object_id
-INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
-WHERE i.Is_Hypothetical = 1
